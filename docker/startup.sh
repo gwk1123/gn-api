fcdce3c9f7d6d0161a82c2175f21238ede4d43c6
gwk1123 <g15625281551@163.com>
2022-01-21 19:10:12 +0800

    docker 更改为host模式

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,4 +1,4 @@
 APPNAME=gn-api
 PORT=19093
 docker build -t $APPNAME .
-docker run -itd --name $APPNAME -p $PORT:$PORT  -v /logs/gn-api:/logs/gn-api $APPNAME
+docker run -itd --name $APPNAME --net=host -p $PORT:$PORT  -v /logs/gn-api:/logs/gn-api $APPNAME
