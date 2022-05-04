e742d7cc73558e6eee0903f54a8059bd2c987daf
gwk1123 <g15625281551@163.com>
2022-05-04 22:18:56 +0800

    调整search 调用返回

--- src/main/java/com/gn/api/web/ctrip/OrderCtripResource.java
+++ src/main/java/com/gn/api/web/ctrip/OrderCtripResource.java
@@ -1,6 +1,6 @@
 package com.gn.api.web.ctrip;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
