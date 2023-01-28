08b59ac3c75ffc5bb6d3537cce1eb19fe4cbb303
gwk1123 <g15625281551@163.com>
2023-01-28 11:22:55 +0800

    01.28.0

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,4 +1,4 @@
 APPNAME=gn-api
 PORT=19093
 docker build -t $APPNAME .
-docker run -itd --name $APPNAME --net=host -p $PORT:$PORT --privileged=true -v /gn/logs/api:/gn/logs/api -v /gn/data/api:/gn/data/api $APPNAME
+docker run -itd --name $APPNAME --net=host -p $PORT:$PORT --privileged=true -v /gn/logs/api:/gn/logs/api -v /gn/logFiles:/gn/logFiles -v /gn/data/api:/gn/data/api $APPNAME
